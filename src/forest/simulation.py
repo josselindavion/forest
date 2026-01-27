2407ec8c25783cc4f40a1681ffb73e6634544fe7
Josselin Davion Jouffre <josselin.davion--jouffre@etu.minesparis.psl.eu>
2026-01-27 16:08:33 +0100

    création classe Grid et des méthodes sans les détailler

--- src/forest/simulation.py
+++ src/forest/simulation.py
@@ -1,3 +1,21 @@
+## CLASSE DE LA GRILLE ##
+class Grid:
+    
+    def __init__(self):
+        pass
+
+    ## Lire le contenu initial de la grille depuis un fichier ##
+    def load_from_file(self, filename):
+        pass
+
+    ## Sauvegarder le contenu de la grille finale dans un fichier ##
+    def save_to_file(self, filename):
+        pass
+
+    ## Calculer la prochaine génération de la grille ##
+    def evolve(self):
+        pass
+
 ## FONCTION DE SIMULATION ##
 def start_simulation():
     print("Simulation started.")
